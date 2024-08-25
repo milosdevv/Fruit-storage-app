d8301ab75cbc7f7caa4f37e2a485a8e049d8f933
MilosDev <milosjovanovicc001@gmail.com>
2024-08-25 20:54:59 +0200

    improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
-import {  } from 'ng2-charts';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
